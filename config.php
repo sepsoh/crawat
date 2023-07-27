8b5cf412f8baf60e5deeba61e6fc8242f1187282
Sepehr <sepehr7160@gmail.com>
2023-07-27 12:32:59 +0330

    upgrade to v2.0

--- config.php
+++ config.php
@@ -5,5 +5,23 @@ return [
         'channels' => [
             'ProxyMTProto','TelMTProto'
         ]
+    ],
+    'ss'=>[
+        'cache_time' => 600,
+        'channels' => [
+            'OutlineVpnOfficial'
+        ]
+    ],
+    'vmess'=>[
+        'cache_time' => 600,
+        'channels' => [
+            'v2Line'
+        ]
+    ],
+    'vless'=>[
+        'cache_time' => 600,
+        'channels' => [
+            'vmessorg'
+        ]
     ]
 ];
