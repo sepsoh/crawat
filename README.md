4a417c59c5a7d23293601b02a67e793c60d974bb
0x6D7366 <MsfNemati1386@gmail.com>
2023-08-15 13:20:47 +0330

    upgrade to v3.0

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ You can add Telegram channels to the panel in the `config.php` file.
   - Vless
 
 
-## Crawat Web Panel v2.0 Preview :
+## Crawat Web Panel v3.0 Preview :
 ![image](https://github.com/sepsoh/crawat/assets/87861266/d268e163-c030-4267-b749-796885fb4d8d)
 ![image](https://github.com/sepsoh/crawat/assets/87861266/28d99139-ebe8-461d-bb74-78df579f0a00)
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
-const card = document.getElementById("card");
+// taking care of forEach object problem in FireFox, Internet Explorer, WaterFox
+if (typeof NodeList.prototype.forEach !== 'function') NodeList.prototype.forEach = Array.prototype.forEach;
+
+const theme = (window.localStorage.getItem('theme') ?? 'light') == 'light' ? 'light' : 'dark';
+const toggle_theme = document.querySelector('#toggle-theme');
+document.querySelector('html').setAttribute('theme', theme);
+toggle_theme.onclick = ev => {
+  const theme = (window.localStorage.getItem('theme') ?? 'light') == 'dark' ? 'light' : 'dark';
+  document.querySelector('html').setAttribute('theme', theme);
+  window.localStorage.setItem('theme', theme);
+};
+
+const container = document.getElementById("container");
+const header = document.getElementById("header");
 const types = document.getElementById("types");
 const providerName = document.getElementById("provider-name");
 const providerNameSub = document.getElementById("provider-name-sub");
@@ -17,232 +30,183 @@ let ss_data = [];
 let vmess_data = [];
 let vless_data = [];
 
+const styleGur = color => header.style.setProperty('--background-gur', color);
 
-
-
-
-
-let styleCard = (color) => {
-    card.style.background = `radial-gradient(circle at 50% 0%, ${color} 36%, white 36%)`;
-    card.querySelectorAll(".types span").forEach((item) => {
-        item.style.backgroundColor = color;
-    });
-};
-
-
-
-function mtproto(){
-    providerName.innerHTML = "Connecting ...";
-    image.src = "assets/img/mtproto.png";
-    if(mtproto_data.length === 0) {
-        fetch('feed.php?type=mtproto')
-            .then(response => response.json())
-            .then(data => {
-                mtproto_data = data
-                mtproto_flush(mtproto_data.result[mtproto_pointer]);
-
-                next.addEventListener("click", () => {
-                    if (types.value === "mtproto") {
-                        mtproto_pointer++;
-                        if (mtproto_pointer === mtproto_data.result.length)
-                            mtproto_pointer = 0;
-                        mtproto_flush(mtproto_data.result[mtproto_pointer]);
-                    } else if (types.value === "ss") {
-                        ss_pointer++;
-                        if (ss_pointer === ss_data.result.length)
-                            ss_pointer = 0;
-                        ss_flush(ss_data.result[ss_pointer]);
-                    }else if (types.value === "vmess") {
-                        vmess_pointer++;
-                        if (vmess_pointer === vmess_data.result.length)
-                            vmess_pointer = 0;
-                        vmess_flush(vmess_data.result[vmess_pointer]);
-                    }else if (types.value === "vless") {
-                        vless_pointer++;
-                        if (vless_pointer === vless_data.result.length)
-                            vless_pointer = 0;
-                        vless_flush(vless_data.result[vless_pointer]);
-                    }
-                });
-
-
-            });
-    }else{
+function mtproto() {
+  providerName.innerHTML = "Connecting ...";
+  image.src = "assets/img/mtproto.png";
+  if(mtproto_data.length === 0) {
+    fetch('feed.php?type=mtproto')
+      .then(response => response.json())
+      .then(data => {
+        mtproto_data = data
         mtproto_flush(mtproto_data.result[mtproto_pointer]);
-    }
 
-
-
-
-
-    styleCard("#adcbe0");
-
+        next.addEventListener("click", () => {
+          if (types.value === "mtproto") {
+            mtproto_pointer++;
+            if (mtproto_pointer === mtproto_data.result.length) mtproto_pointer = 0;
+            mtproto_flush(mtproto_data.result[mtproto_pointer]);
+          } else if (types.value === "ss") {
+            ss_pointer++;
+            if (ss_pointer === ss_data.result.length) ss_pointer = 0;
+            ss_flush(ss_data.result[ss_pointer]);
+          } else if (types.value === "vmess") {
+            vmess_pointer++;
+            if (vmess_pointer === vmess_data.result.length) vmess_pointer = 0;
+            vmess_flush(vmess_data.result[vmess_pointer]);
+          } else if (types.value === "vless") {
+            vless_pointer++;
+            if (vless_pointer === vless_data.result.length) vless_pointer = 0;
+            vless_flush(vless_data.result[vless_pointer]);
+          }
+        });
+      });
+  } else mtproto_flush(mtproto_data.result[mtproto_pointer]);
+  styleGur("rgb(173, 203, 224)");
 }
 
-function mtproto_flush(item){
-    providerName.innerHTML = item.provider + " #"+item.priority;
-    providerNameSub.innerHTML = '';
-    update.innerHTML = Math.ceil((mtproto_data.response_time - item.time)/60) + " minutes ago";
+function mtproto_flush(item) {
+  providerName.innerHTML = item.provider + " #"+item.priority;
+  providerNameSub.innerHTML = '';
+  update.innerHTML = Math.ceil((mtproto_data.response_time - item.time)/60) + " minutes ago";
 
-    // remove all connect event listener
-    const new_connect = connect.cloneNode(true);
-    connect.parentNode.replaceChild(new_connect, connect);
-    connect = new_connect;
+  // remove all connect event listener
+  const new_connect = connect.cloneNode(true);
+  connect.parentNode.replaceChild(new_connect, connect);
+  connect = new_connect;
 
-    connect.innerHTML = "Connect";
-    connect.addEventListener("click",()=>{
-        window.open(item.data);
-    });
+  connect.innerHTML = "Connect";
+  connect.onclick = ev => window.open(item.data);
 }
 
+function ss() {
+  providerName.innerHTML = "Connecting ...";
+  image.src = "assets/img/ss.png";
+  styleGur("rgba(103,177,104,0.37)");
 
-function ss(){
-    providerName.innerHTML = "Connecting ...";
-    image.src = "assets/img/ss.png";
-    styleCard("rgba(103,177,104,0.37)");
-
-    if(ss_data.length === 0) {
-        fetch('feed.php?type=ss')
-            .then(response => response.json())
-            .then(data => {
-                ss_data = data
-                ss_flush(ss_data.result[ss_pointer]);
-            });
-    }else{
+  if (ss_data.length === 0) {
+    fetch('feed.php?type=ss')
+      .then(response => response.json())
+      .then(data => {
+        ss_data = data
         ss_flush(ss_data.result[ss_pointer]);
-    }
-
+      });
+  } else ss_flush(ss_data.result[ss_pointer]); 
 }
-function ss_flush(item){
-    providerName.innerHTML = item.provider + " #"+item.priority;
-    providerNameSub.innerHTML = item.tag;
-    update.innerHTML = Math.ceil((ss_data.response_time - item.time)/60) + " minutes ago";
+function ss_flush(item) {
+  providerName.innerHTML = item.provider + " #"+item.priority;
+  providerNameSub.innerHTML = item.tag;
+  update.innerHTML = Math.ceil((ss_data.response_time - item.time)/60) + " minutes ago";
 
-    // remove all connect event listener
-    const new_connect = connect.cloneNode(true);
-    connect.parentNode.replaceChild(new_connect, connect);
-    connect = new_connect;
+  // remove all connect event listener
+  const new_connect = connect.cloneNode(true);
+  connect.parentNode.replaceChild(new_connect, connect);
+  connect = new_connect;
 
-    connect.innerHTML = "Copy";
-    connect.addEventListener("click",()=>{
-        navigator.clipboard.writeText(item.data);
-        connect.innerHTML = "Copied";
-        setTimeout(()=>{
-            connect.innerHTML = "Copy";
-        },2000);
-
-    });
+  connect.innerHTML = "Copy";
+  connect.onclick = ev => {
+    navigator.clipboard.writeText(item.data);
+    connect.innerHTML = "Copied";
+    setTimeout(() => {
+      connect.innerHTML = "Copy";
+    }, 2000);
+  };
 }
 
-function vmess(){
-    providerName.innerHTML = "Connecting ...";
-    image.src = "assets/img/vmess.png";
-    styleCard("rgba(84,100,136,0.37)");
+function vmess() {
+  providerName.innerHTML = "Connecting ...";
+  image.src = "assets/img/vmess.png";
+  styleGur("rgba(84,100,136,0.37)");
 
-    if(vmess_data.length === 0) {
-        fetch('feed.php?type=vmess')
-            .then(response => response.json())
-            .then(data => {
-                vmess_data = data
-                vmess_flush(vmess_data.result[vmess_pointer]);
-            });
-    }else{
+  if (vmess_data.length === 0) {
+    fetch('feed.php?type=vmess')
+      .then(response => response.json())
+      .then(data => {
+        vmess_data = data
         vmess_flush(vmess_data.result[vmess_pointer]);
-    }
-
-}
-function vmess_flush(item){
-    providerName.innerHTML = item.provider + " #"+item.priority;
-    providerNameSub.innerHTML = item.tag;
-    update.innerHTML = Math.ceil((vmess_data.response_time - item.time)/60) + " minutes ago";
-
-    // remove all connect event listener
-    const new_connect = connect.cloneNode(true);
-    connect.parentNode.replaceChild(new_connect, connect);
-    connect = new_connect;
-
-    connect.innerHTML = "Copy";
-    connect.addEventListener("click",()=>{
-        navigator.clipboard.writeText(item.data);
-        connect.innerHTML = "Copied";
-        setTimeout(()=>{
-            connect.innerHTML = "Copy";
-        },2000);
-
-    });
+      });
+    } else vmess_flush(vmess_data.result[vmess_pointer]);
 }
 
-function vless(){
-    providerName.innerHTML = "Connecting ...";
-    image.src = "assets/img/vless.png";
-    styleCard("rgba(136,84,121,0.37)");
+function vmess_flush(item) {
+  providerName.innerHTML = item.provider + " #"+item.priority;
+  providerNameSub.innerHTML = item.tag;
+  update.innerHTML = Math.ceil((vmess_data.response_time - item.time)/60) + " minutes ago";
 
-    if(vless_data.length === 0) {
-        fetch('feed.php?type=vless')
-            .then(response => response.json())
-            .then(data => {
-                vless_data = data
-                vless_flush(vless_data.result[vless_pointer]);
-            });
-    }else{
+  // remove all connect event listener
+  const new_connect = connect.cloneNode(true);
+  connect.parentNode.replaceChild(new_connect, connect);
+  connect = new_connect;
+
+  connect.innerHTML = "Copy";
+  connect.onclick = ev => {
+    navigator.clipboard.writeText(item.data);
+    connect.innerHTML = "Copied";
+    setTimeout(() => {
+      connect.innerHTML = "Copy";
+    }, 2000);
+  };
+}
+
+function vless() {
+  providerName.innerHTML = "Connecting ...";
+  image.src = "assets/img/vless.png";
+  styleGur("rgba(136,84,121,0.37)");
+
+  if (vless_data.length === 0) {
+    fetch('feed.php?type=vless')
+      .then(response => response.json())
+      .then(data => {
+        vless_data = data
         vless_flush(vless_data.result[vless_pointer]);
-    }
-
+      });
+    } else vless_flush(vless_data.result[vless_pointer]);
 }
-function vless_flush(item){
-    providerName.innerHTML = item.provider + " #"+item.priority;
-    providerNameSub.innerHTML = item.tag;
-    update.innerHTML = Math.ceil((vless_data.response_time - item.time)/60) + " minutes ago";
+function vless_flush(item) {
+  providerName.innerHTML = item.provider + " #"+item.priority;
+  providerNameSub.innerHTML = item.tag;
+  update.innerHTML = Math.ceil((vless_data.response_time - item.time)/60) + " minutes ago";
 
-    // remove all connect event listener
-    const new_connect = connect.cloneNode(true);
-    connect.parentNode.replaceChild(new_connect, connect);
-    connect = new_connect;
+  // remove all connect event listener
+  const new_connect = connect.cloneNode(true);
+  connect.parentNode.replaceChild(new_connect, connect);
+  connect = new_connect;
 
-    connect.innerHTML = "Copy";
-    connect.addEventListener("click",()=>{
-        navigator.clipboard.writeText(item.data);
-        connect.innerHTML = "Copied";
-        setTimeout(()=>{
-            connect.innerHTML = "Copy";
-        },2000);
-
-    });
+  connect.innerHTML = "Copy";
+  connect.onclick = ev => {
+    navigator.clipboard.writeText(item.data);
+    connect.innerHTML = "Copied";
+    setTimeout(() => {
+      connect.innerHTML = "Copy";
+    }, 2000);
+  };
 }
-
 
 fetch('feed.php?list')
-    .then(response => response.json())
-    .then(data => {
-        const typeList = {
-            "mtproto": "MTProto",
-            "ss": "Shadowsocks",
-            "vmess" : "Vmess",
-            "vless" : "Vless"
-        }
-        data.result.forEach(type => {
-            const opt =  document.createElement('option');
-            opt.value = type;
-            opt.text = typeList[type];
-            types.add(opt);
-        });
-        //first call
-        mtproto();
+  .then(response => response.json())
+  .then(data => {
+    const type_list = {
+      "mtproto": "MTProto",
+      "ss"     : "Shadowsocks",
+      "vmess"  : "Vmess",
+      "vless"  : "Vless"
+    };
 
-        types.addEventListener("change", () => {
-            if(types.value === "mtproto")
-                mtproto();
-            else if(types.value === "ss")
-                ss();
-            else if (types.value === "vmess")
-                vmess();
-            else if (types.value === "vless")
-                vless();
-
-
-        });
+    data.result.forEach(type => {
+      const opt =  document.createElement('option');
+      opt.value = type;
+      opt.text = type_list[type];
+      types.add(opt);
     });
 
+    // first call
+    mtproto();
 
-
-
+    types.addEventListener("change", () => {
+      if(types.value === "mtproto") mtproto();
+      else if (types.value === "vmess") vmess();
+      else if (types.value === "vless") vless();
+      else if(types.value === "ss") ss();
+    });
+  });
